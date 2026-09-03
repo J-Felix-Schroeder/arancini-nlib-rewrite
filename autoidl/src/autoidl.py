1c7f49c775fdc4ba8ef78e86444ab1546fde323f
felix <felix.schroeder@rwth-aachen.de>
2026-09-03 11:28:11 +0200

    autoidl translate libc gated behind flag

--- autoidl/src/autoidl.py
+++ autoidl/src/autoidl.py
@@ -6,12 +6,13 @@ from dwarfhelper import get_signatures
 import dwarfhelper
 from aidlsig import AidlLibrary, AidlSignature
 from flibmaker import build_flib, translate_flib, translate_libc
-from pathman import get_idl_path, get_musl_auto_lid_path
+from pathman import get_idl_path, get_musl_auto_lid_path, get_translated_path
 from run import run_txlat
 import analytics
 
 analyze = False
 struct_check = True
+libc = False
 
 def analyze_lib(library, symbols):
     analytics.start()
@@ -55,7 +56,9 @@ def process_libc():
         f.write(str(library))
     if analyze:
         return None
-    return translate_libc()
+    if libc:
+        return translate_libc()
+    return get_translated_path("libc.so")
 
 def translate_elf(elf_path, libs, out_path):
     flags = []
@@ -104,9 +107,12 @@ def main():
     p.add_argument("-o", "--output", nargs="?")
     p.add_argument("--anal", action="store_true")
     p.add_argument("--no-struct-check", action="store_true")
+    p.add_argument("--translate-libc", action="store_true")
     args = p.parse_args()
     global struct_check
     struct_check = not args.no_struct_check
+    global libc
+    libc = args.translate_libc
     global analyze
     analyze = args.anal
     
